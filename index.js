9073c3308819ff31581dd8f90e8add75a5d19fb7
Hamdiibrahim <yusufhamdi137@gmail.com>
2024-09-17 12:09:19 +0300

    parameters assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+function introduction(Aki){
+    return `Hi, my name is ${Aki}.`;
+}
+function introductionWithLanguage(Aki,Javascript){
+    return `Hi, my name is ${Aki} and I am learning to program in ${Javascript}.`;
+}
+function introductionWithLanguageOptional(Aki,Javascript = 'JavaScript'){
+    return `Hi, my name is ${Aki} and I am learning to program in ${Javascript}.`;
+}
